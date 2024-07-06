c597f250f02923efd7cc30f75df582505bfe6211
gurbanovnihad <7404@holbertonstudents.com>
2024-07-06 20:07:33 +0400

    course info step 1 and 2

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,9 @@
+import { Part } from "./Part"
+
+export const Content = (props) => {
+  return <>
+    <Part part = {props.part1} exercise = {props.exercise1} />
+    <Part part = {props.part2} exercise = {props.exercise2} />
+    <Part part = {props.part3} exercise = {props.exercise3} />
+    </>
+}
---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,5 @@
+export const Total = (props) => {
+  return <p>
+    Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}
+  </p>
+}
